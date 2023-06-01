15322a641897051b5e6c554dcb788147536511e5
Armando Valencia <javm8@yahoo.com>
2023-05-31 22:19:00 -0500

    Add sorting by name and by completed tasks

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -17,16 +17,51 @@ export const TodoItem = ({ todoItem, todos, setTodos }: TodoItemProps) => {
 
 	// Mark a todo item as done
 	const handleComplete = (id: string) => {
-		setTodos(
-			todos.map((todo) => {
-				if (todo.id !== id) return todo;
-				// If the todo item id matches the id of the seleted item, return the todo item with the isComplete property set to the opposite of what it was before
-				return {
-					...todo,
-					isComplete: !todo.isComplete,
-				};
-			})
-		);
+		// Use the callback version of setTodos
+		setTodos((prev) => {
+			// Map over the todos array and toggle the isComplete property of the todo with the matching id
+			const tempTodos = prev.map((todo) => {
+				if (todo.id === id)
+					return { ...todo, isComplete: !todo.isComplete };
+
+				return todo;
+			});
+
+			const incompleteTodos: Todo[] = [];
+			const completeTodos: Todo[] = [];
+
+			// Split the todos into two arrays: completed and incomplete
+			tempTodos.map((item) => {
+				if (item.isComplete) {
+					completeTodos.push(item);
+				} else {
+					incompleteTodos.push(item);
+				}
+			});
+
+			// Sort the incomplete todos alphabetically
+			const sortIncomplete = incompleteTodos.sort((a, b) => {
+				const fa = a.name.toLowerCase(),
+					fb = b.name.toLowerCase();
+
+				if (fa < fb) return -1;
+				if (fa > fb) return 1;
+				return 0;
+			});
+
+			// Sort the completed todos alphabetically
+			const sortCompleted = completeTodos.sort((a, b) => {
+				const fa = a.name.toLowerCase(),
+					fb = b.name.toLowerCase();
+
+				if (fa < fb) return -1;
+				if (fa > fb) return 1;
+				return 0;
+			});
+
+			// Return all todos sorted alphabetically with the incomplete ones being first
+			return sortIncomplete.concat(sortCompleted);
+		});
 	};
 
 	// Delete a todo item
@@ -57,9 +92,7 @@ export const TodoItem = ({ todoItem, todos, setTodos }: TodoItemProps) => {
 		<form
 			onSubmit={(e) => handleEdit(e, todoItem.id)}
 			className={`flex rounded-md p-3 my-1 text-neutral-200 justify-between w-full space-x-1 ${
-				todoItem.isComplete
-					? "bg-emerald-700 animate-bounce-short"
-					: "bg-blue-600"
+				todoItem.isComplete ? "bg-emerald-700" : "bg-blue-600"
 			}`}
 		>
 			{isEditing ? (
@@ -84,7 +117,9 @@ export const TodoItem = ({ todoItem, todos, setTodos }: TodoItemProps) => {
 					onClick={() => {
 						setIsEditing(!isEditing);
 					}}
-					className="cursor-pointer hover:scale-110 duration-200 hover:text-black"
+					className={`cursor-pointer hover:scale-110 duration-200 hover:text-black ${
+						isEditing ? "animate-pulse text-black font-bold" : ""
+					}}`}
 				>
 					<Edit />
 				</span>
